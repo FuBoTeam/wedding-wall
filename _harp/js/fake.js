0a8f4b43b49e71c6c7d90cbe42f61c61873b67a6
ElaineHuang <starbrave1313677@gmail.com>
2018-05-06 18:20:53 +0800

    add quene

--- _harp/js/fake.js
+++ _harp/js/fake.js
@@ -93,3 +93,17 @@ const backgroundImages = [
     "url": "https://storage.googleapis.com/wedding_iota/30_small.jpg",
   }
 ];
+
+const users = [{
+  name: 'Elaine Huang',
+  message: 'HiHi~Happy days',
+  img_url: "https://storage.googleapis.com/wedding_iota/26_small.jpg"
+}, {
+  name: 'Ben Liu',
+  message: 'HiHi~Happy days',
+  img_url: "https://storage.googleapis.com/wedding_iota/5_small.jpg"
+}, {
+  name: 'Orga',
+  message: 'HiHi~Happy days',
+  img_url: "https://storage.googleapis.com/wedding_iota/9_small.jpg"
+}];
--- js/index.js
+++ js/index.js
@@ -22,19 +22,54 @@ const showDialog = (user) => {
   $('#dialog').addClass('show-dialog');
   $('.modal-sm').html(`
     <div class="image-container">
-      <img class="user-image" src="https://storage.googleapis.com/wedding_iota/0.jpg">
+      <img class="user-image" src="${user.img_url}">
     </div>
     <div class="message">
       <h2>@ ${user.name}<h2>
       <p>${user.message}</p>
     </div>
   `);
+
+  setTimeout(() => {
+    $('#dialog').removeClass('show-dialog');
+  }, 3000);
+}
+
+class Quene {
+  constructor() {
+    this.q = [];
+  }
+
+  push(element) {
+    this.q.push(element);
+  }
+
+  pop() {
+    const result = this.q.splice(0, 1)[0];
+    return result;
+  }
+
+  isEmpty() {
+    return this.q.length === 0;
+  }
+}
+
+const pickUpFeed = (newFeeds, oldFeeds) => {
+  const nextFeed = newFeeds.isEmpty() ? oldFeeds.pop() : newFeeds.pop();
+  showDialog(nextFeed);
+  oldFeeds.push(nextFeed);
 }
 
 $(document).ready(function() {
   displayBackground();
-  showDialog({
-    name: 'Elaine Huang',
-    message: 'Yeah! Demo! CSS Day~!'
+  const newFeeds = new Quene();
+  const oldFeeds = new Quene();
+
+  users.forEach((user) => {
+    newFeeds.push(user);
   });
+
+  setInterval(() => {
+    pickUpFeed(newFeeds, oldFeeds)
+  }, 5000);
 });
